db8f65638e082ed7e50b06cbf3508491198429d2
Vladimir <vlabum@gmail.com>
2018-09-18 23:33:21 +0500

    Создание классов по ДЗ 1

--- javacoreadv/src/main/java/ru/vlabum/se/homework/Course.java
+++ javacoreadv/src/main/java/ru/vlabum/se/homework/Course.java
@@ -1,4 +1,29 @@
 package ru.vlabum.se.homework;
 
+/**
+ * 3. Добавить класс Course (полоса препятствий),
+ * в котором будут находиться массив препятствий и метод,
+ * который будет просить команду пройти всю полосу;
+ */
+
 public class Course {
+
+    private int[] relayRace; // эстафета
+
+    public Course(int[] relayRace) {
+        this.relayRace = relayRace;
+    }
+
+    public void doIt(Team team) {
+        for (Player p : team.getPlayers()) {
+            int len = 0;
+            for (int i = 0; i < relayRace.length; i++) {
+                len += relayRace[i];
+            }
+            p.doIt(len);
+        }
+    }
+
+
+
 }
--- javacoreadv/src/main/java/ru/vlabum/se/homework/Player.java
+++ javacoreadv/src/main/java/ru/vlabum/se/homework/Player.java
@@ -1,4 +1,41 @@
 package ru.vlabum.se.homework;
 
 public class Player {
+
+    private int maxDistance;
+    public int getMaxDistance() {
+        return maxDistance;
+    }
+
+    private String namePlayer;
+    public String getNamePlayer() {
+        return namePlayer;
+    }
+
+    private boolean isDoIt = false;
+    public boolean getIsDoIt() {
+        return isDoIt;
+    }
+    public void setDoIt(boolean isDoIt) {
+        this.isDoIt = isDoIt;
+    }
+
+    /**
+     * Игроки создаются с разбегом возможностей по дистанции от 2000 (условных метров), до 4000
+     * @param   namePlayer
+     *          Имя игрока
+     */
+    public Player(String namePlayer) {
+        this.namePlayer = namePlayer;
+        maxDistance = 2000 + (int)(Math.random() * 2000);
+    }
+
+    @Override
+    public String toString() {
+        return namePlayer + ", предельная дистанция: " + maxDistance;
+    }
+
+    public void doIt(int len) {
+        this.isDoIt = (len <= maxDistance);
+    }
 }
